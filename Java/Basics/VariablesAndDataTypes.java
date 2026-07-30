e5b1bea7d35cf0cd1e2507578e72b9ec8a9439f4
Nibrit Berlin B <161574244+nibrit@users.noreply.github.com>
2026-07-30 16:03:02 +0530

    feat(java): add variables and primitive data types example

---
+++ Java/Basics/VariablesAndDataTypes.java
@@ -0,0 +1,25 @@
+public class VariablesAndDataTypes {
+
+    public static void main(String[] args) {
+
+        byte age = 21;
+        short year = 2026;
+        int salary = 50000;
+        long population = 1400000000L;
+
+        float height = 195.0f;
+        double price = 999.99;
+
+        char grade = 'A';
+        boolean isLearningJava = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Salary: " + salary);
+        System.out.println("Population: " + population);
+        System.out.println("Height: " + height);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Learning Java: " + isLearningJava);
+    }
+}
