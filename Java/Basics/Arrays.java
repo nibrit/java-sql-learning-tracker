6b806b876cadf8503c3aef324400a3625ec5a103
Nibrit Berlin B <161574244+nibrit@users.noreply.github.com>
2026-08-08 13:39:25 +0530

    Create Arrays.java

---
+++ Java/Basics/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements:");
+
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\nLength: " + numbers.length);
+    }
+}
