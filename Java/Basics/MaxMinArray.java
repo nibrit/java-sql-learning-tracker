4b34a2d5855d86b3c61fbee45a30607181327967
Nibrit Berlin B <161574244+nibrit@users.noreply.github.com>
2026-08-08 13:41:13 +0530

    Add MaxMinArray class to find array extremes
    Implement a program to find the maximum and minimum values in an array.

---
+++ Java/Basics/MaxMinArray.java
@@ -0,0 +1,24 @@
+public class MaxMinArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 45, 7, 89, 23};
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int num : numbers) {
+
+            if (num > max) {
+                max = num;
+            }
+
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
