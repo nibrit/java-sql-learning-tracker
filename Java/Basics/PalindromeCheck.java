ae044f99573ca0f92ceb44c434cf3b1bb766e4fa
Nibrit Berlin B <161574244+nibrit@users.noreply.github.com>
2026-08-09 13:29:55 +0530

    Add PalindromeCheck class to verify palindromes

---
+++ Java/Basics/PalindromeCheck.java
@@ -0,0 +1,19 @@
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
